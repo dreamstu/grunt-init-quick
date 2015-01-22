5646c4ba4fbbaa8a93761e2d979f19ec346f8117
dreamstu <cjxsgn@163.com>
2015-01-22 09:38:01 +0800

    first commit

--- root/README.md
+++ root/README.md
@@ -3,19 +3,15 @@
 {%= description %}
 
 ## Getting Started
-Download the [production version][min] or the [development version][max].
-
-[min]: https://raw.github.com/{%= git_user %}/{%= git_repo %}/master/dist/{%= name %}.min.js
-[max]: https://raw.github.com/{%= git_user %}/{%= git_repo %}/master/dist/{%= name %}.js
 
 In your web page:
 
 ```html
 <script src="jquery.js"></script>
-<script src="dist/{%= name %}.min.js"></script>
+<script src="seajs.js"></script>
 <script>
-jQuery(function($) {
-  $.awesome(); // "awesome"
+seajs.use('{%= name %}/{%= version %}/{%= name %}',function({%= name %}) {
+  // "use {%= name %}"
 });
 </script>
 ```
--- template.js
+++ template.js
@@ -1,28 +1,37 @@
 /*
- * grunt-init-jquery
+ * grunt-init-quick
  * https://gruntjs.com/
  *
- * Copyright (c) 2013 "Cowboy" Ben Alman, contributors
+ * Copyright (c) 2015 "QuickJs" dreamstu, contributors
  * Licensed under the MIT license.
  */
 
 'use strict';
 
+var $extend = function (target, source) {
+    for (var p in source) {
+        if (source.hasOwnProperty(p)) {
+            target[p] = source[p];
+        }
+    }
+    return target;
+};
+
 // Basic template description.
-exports.description = 'Create a jQuery plugin, including QUnit unit tests.';
+exports.description = 'Create a QuickJs plugin, including QUnit unit tests.';
 
 // Template-specific notes to be displayed before question prompts.
-exports.notes = '_Project name_ should not contain "jquery" or "js" and ' +
-  'should be a unique ID not already in use at plugins.jquery.com. _Project ' +
+exports.notes = '_Project name_ should not contain "quick" or "js" and ' +
+  'should be a unique ID not already in use at plugins.quickjs.org.\n _Project ' +
   'title_ should be a human-readable title, and doesn\'t need to contain ' +
-  'the word "jQuery", although it may. For example, a plugin titled "Awesome ' +
+  'the word "Quick", although it may.\n For example, a plugin titled "Awesome ' +
   'Plugin" might have the name "awesome-plugin".' +
   '\n\n'+
   'For more information, please see the following documentation:' +
   '\n\n'+
-  'Naming Your Plugin      http://plugins.jquery.com/docs/names/\n' +
-  'Publishing Your Plugin  http://plugins.jquery.com/docs/publish/\n' +
-  'Package Manifest        http://plugins.jquery.com/docs/package-manifest/';
+  'Naming Your Plugin      http://plugins.quickjs.org/docs/names/\n' +
+  'Publishing Your Plugin  http://plugins.quickjs.org/docs/publish/\n' +
+  'Package Manifest        http://plugins.quickjs.org/docs/package-manifest/';
 
 // Template-specific notes to be displayed after question prompts.
 exports.after = 'You should now install project dependencies with _npm ' +
@@ -42,11 +51,11 @@ exports.template = function(grunt, init, done) {
     // Prompt for these values.
     init.prompt('name'),
     init.prompt('title', function(value, data, done) {
-      // Fix jQuery capitalization.
-      value = value.replace(/jquery/gi, 'jQuery');
+      // Fix Quick capitalization.
+      value = value.replace(/jquery/gi, 'Quick');
       done(null, value);
     }),
-    init.prompt('description', 'The best jQuery plugin ever.'),
+    init.prompt('description', 'The best QuickJs plugin ever.'),
     init.prompt('version'),
     init.prompt('repository'),
     init.prompt('homepage'),
@@ -55,11 +64,9 @@ exports.template = function(grunt, init, done) {
     init.prompt('author_name'),
     init.prompt('author_email'),
     init.prompt('author_url'),
-    init.prompt('jquery_version')
+    init.prompt('seajs_version','2.2.1')
   ], function(err, props) {
     // A few additional properties.
-    props.jqueryjson = props.name + '.jquery.json';
-    props.dependencies = {jquery: props.jquery_version || '>= 1'};
 
     props.keywords = [];
 
@@ -73,25 +80,24 @@ exports.template = function(grunt, init, done) {
     init.copyAndProcess(files, props, {noProcess: 'libs/**'});
 
     // Generate package.json file, used by npm and grunt.
-    init.writePackageJSON('package.json', {
-      name: 'jquery-plugin',
+    init.writePackageJSON('package.json',$extend({
+      name: 'quick-plugin',
       version: '0.0.0-ignored',
       npm_test: 'grunt qunit',
       // TODO: pull from grunt's package.json
       node_version: '>= 0.8.0',
       devDependencies: {
-        'grunt-contrib-jshint': '~0.10.0',
-        'grunt-contrib-qunit': '~0.2.0',
-        'grunt-contrib-concat': '~0.3.0',
-        'grunt-contrib-uglify': '~0.2.0',
-        'grunt-contrib-watch': '~0.4.0',
+        'grunt-quick-transport': '~0.1.0',
+        'grunt-cmd-concat': '~0.2.7',
         'grunt-contrib-clean': '~0.4.0',
-      },
-    });
-
-    // Generate jquery.json file.
-    init.writePackageJSON(props.jqueryjson, props, function(pkg, props) {
-      // The jQuery site needs the "bugs" value as a string.
+        'grunt-contrib-uglify': '~0.2.0',
+        'grunt-contrib-copy': '~0.7.0',
+        'grunt-contrib-watch': '~0.4.0',
+        'load-grunt-tasks': '^0.4.0',
+        'time-grunt': '^0.4.0'
+      }
+    },props), function(pkg, props) {
+      // The QuickJs site needs the "bugs" value as a string.
       if ('bugs' in props) { pkg.bugs = props.bugs; }
       return pkg;
     });
